6205fde78d07cb04e998d695c95da22e4257f301
Charles Leon <charles.p.leon@gmail.com>
2019-05-01 00:50:31 -0700

    Setup proper structure for data storage

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 const winston = require('winston');
+const moment = require('moment');
 
 //Globals
 const app = express();
 const port = process.env.PORT;
 const DB_URL = process.env.MONGO_URI;
-var DB;
+var db;
 
 //Middleware
 app.use(cors());
@@ -25,21 +27,16 @@ const LOGGER = winston.createLogger({
     ]
 });
 
-app.listen(port, () =>
-    LOGGER.log({
-        level: 'info',
-        message: 'Server restarted'
-    })
-);
-
-const MONGO_CLIENT = new MongoClient(DB_URL, { useNewUrlParser: true });
-MONGO_CLIENT.connect(err => {
-    if (err) {
-        console.log(err);
-    } else {
-        DB = MONGO_CLIENT.db("grouch");
-    }
-});
+mongoose.set('debug', true);
+mongoose.connect(DB_URL, {useNewUrlParser: true}).then(connectedDB => {
+    db = connectedDB;
+    app.listen(port, () =>
+        LOGGER.log({
+            level: 'info',
+            message: 'Server restarted'
+        })
+    );
+})
 
 /*
  * Primary Routes
@@ -48,13 +45,22 @@ app.get('/', function (req, res, next) {
     res.status(200).send("OK");
 });
 
-app.post('/test', function (req, res, next) {
-    DB.collection('trash_level').insert({calculated_level: 2, raw_distance: 2}, function(err, r) {
-        if (err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send("OK");
+var openSchema = new Schema({}, { strict: false });
+var Container = mongoose.model('container', openSchema);
+app.post('/heightRecord/:containerId', function (req, res, next) {
+    let timeStampKey = `heightRecords.${moment().unix()}`;
+    Container.updateOne(
+        { _id: req.params.containerId },
+        {
+            $push: {
+                wasteLevels: req.body.distance,
+                wasteTimes: moment().format("YYYY-MM-DD HH:mm:ss")
+            }
         }
-    });
+    ).then(() => {
+        res.status(200).send("OK");
+    }).catch(err => {
+        res.status(500).send(err);
+    })
 });
 
